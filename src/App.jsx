2e02968b949faa04ecf3fd87ef78fd1f1bdf0ba1
chaturvedi-anil <chaturvedia560@gmail.com>
2024-12-21 12:47:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900'>
+    </div>  
+  )
+}
+
+export default App
